064ed52e790081f65ec3fee0e3be7dfdc5d2acb0
Joseph-Vanliew <joevanliew@gmail.com>
2024-09-10 11:55:55 -0400

    Added IP address printout during intial server run

--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
@@ -9,7 +10,9 @@ import java.util.Date;
 public class Server {
     public static void main(String[] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(7689)) {
-            System.out.println("The Server is running...");
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            System.out.println("The Server is running on IP: " + inetAddress.getHostAddress());
+
 
             while (true) {
                 // Wait for a client to connect
